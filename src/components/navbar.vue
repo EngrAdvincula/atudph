76680e8f08eadfd46642efe7a9a2ed2d7b164d69
Daniel A. Soriano II <72549944+EngrJill@users.noreply.github.com>
2021-07-26 17:41:35 +0800

    Coded na navbar with RWD burger at 480px or phone size

---
+++ src/components/navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="try">
+      <div class="nav">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    .try {
+        .nav {
+            width: 100%;
+            height: 60px;
+            background-color: #2da81a;
+            border-bottom: 1px solid #4e4e4e;
+        }
+    }
+</style>
